04533af600d82c2711527e903452f22b26e4076d
Daniel-Alievsky <daniel.alievsky@gmail.com>
2013-12-03 15:50:42 +0200

    навожу порядок

--- lib/html/publish.bat
+++
@@ -1,7 +0,0 @@
-copy SimpleImageIntroduction_ru.html \WebWarper\wwgz\f_internal\webwarpernetcfg\ROOT\algart.net-80\html\java\AlgART\docs\tutorials\
-copy connectivity_theorem.html \WebWarper\wwgz\f_internal\webwarpernetcfg\ROOT\algart.net-80\html\ru\skeletonization_algorithms\
-copy ..\..\SimagisMatrixDemo\src\com\simagis\matrix\demo\plugin3\SimplestTutorialDemo.java \WebWarper\wwgz\f_internal\webwarpernetcfg\ROOT\algart.net-80\html\java\AlgART\docs\tutorials\
-pushd %cd%
-cd \WebWarper\cgi-bin\wwcgi\f_internal\util\
-call makeww
-popd
