86b6fa8df553ef9de55bc9fb57452bb410594967
Daniel Alievsky <daniel.alievsky@gmail.com>
2014-03-23 16:07:20 +0200

    better build.xml, extracting separate IDEA module algart2003.iml

--- build.cmd
+++ build.cmd
@@ -1,3 +1,2 @@
-@rem set JAVA_HOME=%ProgramFiles%\Java\jdk1.6.0_16
 @call ant
 
--- compile15.cmd
+++ compile15.cmd
@@ -1,4 +1,4 @@
-@set JDK_VERSION=jdk1.5.0_17
+@set JDK_VERSION=jdk1.5.0_22
 @set JAVA_HOME=%ProgramFiles%\Java\%JDK_VERSION%
 @if not "%ProgramFiles(x86)%"=="" set JAVA_HOME=%ProgramFiles(x86)%\Java\%JDK_VERSION%
 @call ant compile
