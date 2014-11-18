f6f8677670862c7c6add9e04f85c87bd0dd01082
Daniel Alievsky <daniel.alievsky@gmail.com>
2014-11-18 17:55:46 +0200

    restored cmd-file for testing package sealing

---
+++ additional/testBadClassViolatingAlgARTSealing.cmd
@@ -0,0 +1,2 @@
+call "%JAVA_HOME%\bin\javac" -cp ../algart.jar -d classes src/net/algart/arrays/BadClassViolatingAlgARTSealing.java
+call "%JAVA_HOME%\bin\java" -cp classes;../algart.jar net.algart.arrays.BadClassViolatingAlgARTSealing
