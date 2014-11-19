a58ea844a5f9fa61856618fe4c801a8a7cec2382
Daniel Alievsky <daniel.alievsky@gmail.com>
2014-11-19 17:55:37 +0200

    test for sealing violation corrected for new directories structure

--- additional/testBadClassViolatingAlgARTSealing.cmd
+++ additional/testBadClassViolatingAlgARTSealing.cmd
@@ -1,2 +1,4 @@
-call "%JAVA_HOME%\bin\javac" -cp ../algart.jar -d classes src/net/algart/arrays/BadClassViolatingAlgARTSealing.java
-call "%JAVA_HOME%\bin\java" -cp classes;../algart.jar net.algart.arrays.BadClassViolatingAlgARTSealing
+call "%JAVA_HOME%\bin\javac" -cp ../target/ant-jars/algart.jar -d ../target/test-classes ../src/test/java/net/algart/arrays/BadClassViolatingAlgARTSealing.java
+rem - It is compiled
+call "%JAVA_HOME%\bin\java" -cp ../target/ant-jars/algart.jar;../target/test-classes net.algart.arrays.BadClassViolatingAlgARTSealing
+rem - But it leads to exception "sealing violation"
