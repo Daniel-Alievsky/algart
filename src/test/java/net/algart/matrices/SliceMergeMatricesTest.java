0ed3e76b62ad9030c090023aa8183e046945438d
Daniel Alievsky <daniel.alievsky@gmail.com>
2024-03-06 19:15:55 +0200

    SliceMergeMatricesTest: better testing

--- src/test/java/net/algart/matrices/SliceMergeMatricesTest.java
+++ src/test/java/net/algart/matrices/SliceMergeMatricesTest.java
@@ -33,7 +33,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class SliceMergeMatricesTest {
-    private static ArrayContext CONTEXT = ArrayContext.DEFAULT_SINGLE_THREAD;
+    private static final boolean MULTIHREADING = false;
+    private static final MemoryModel MEMORY_MODEL = SimpleMemoryModel.getInstance();
 
     private static void checkIdentity(Matrix<?> a, Matrix<?> b, String what) {
         if (!a.dimEquals(b)) {
@@ -51,6 +52,7 @@ public class SliceMergeMatricesTest {
         }
         final Path sourceFile = Paths.get(args[0]);
         final Path matrixFolder = Paths.get(args[1]);
+        final ArrayContext context = ArrayContext.getSimpleContext(MEMORY_MODEL, MULTIHREADING);
         List<Matrix<? extends PArray>> source = MatrixIO.readImage(sourceFile);
         List<Matrix<? extends PNumberArray>> matrices = Matrices.several(
                 PNumberArray.class,
@@ -58,7 +60,7 @@ public class SliceMergeMatricesTest {
         // - matrices, loaded by MatrixIO, are not binary
         System.out.printf("List: %s%n", matrices);
         Matrix<PArray> merged = Matrices.mergeLayers(Arrays.SMM, matrices);
-        System.out.printf("Joined: %s%n", merged);
+        System.out.printf("Merged: %s%n", merged);
         MatrixIO.writeAlgARTImage(matrixFolder, List.of(merged));
         List<Matrix<PArray>> unpacked = Matrices.asLayers(merged);
         for (int k = 0; k < unpacked.size(); k++) {
@@ -67,9 +69,9 @@ public class SliceMergeMatricesTest {
         for (int test = 1; test <= 10; test++) {
             System.out.printf("%nTest #%d%n", test);
             long t1 = System.nanoTime();
-            Matrix<PNumberArray> interleaved = Matrices.interleave(CONTEXT, matrices);
+            Matrix<PNumberArray> interleaved = Matrices.interleave(context, matrices);
             long t2 = System.nanoTime();
-            List<Matrix<PArray>> separated = Matrices.separate(CONTEXT, interleaved);
+            List<Matrix<PArray>> separated = Matrices.separate(context, interleaved);
             long t3 = System.nanoTime();
             for (int k = 0; k < unpacked.size(); k++) {
                 checkIdentity(separated.get(k), matrices.get(k), "Channel #" + k);
@@ -79,12 +81,12 @@ public class SliceMergeMatricesTest {
             System.out.printf("separate: %.3f ms, %.3f MB/sec%n",
                     (t3 - t2) * 1e-6, Matrices.sizeOf(interleaved) / 1048576.0 / ((t3 - t2) * 1e-9));
             List<Matrix<UpdatablePArray>> separatedResult = separated.stream().map(
-                    m -> Arrays.SMM.newMatrix(UpdatablePArray.class, m)).toList();
-            Matrix<UpdatablePArray> interleavedResult = Arrays.SMM.newMatrix(UpdatablePArray.class, interleaved);
+                    m -> MEMORY_MODEL.newMatrix(UpdatablePArray.class, m)).toList();
+            Matrix<UpdatablePArray> interleavedResult = MEMORY_MODEL.newMatrix(UpdatablePArray.class, interleaved);
             t1 = System.nanoTime();
-            Matrices.separate(CONTEXT, separatedResult, interleaved);
+            Matrices.separate(context, separatedResult, interleaved);
             t2 = System.nanoTime();
-            Matrices.interleave(CONTEXT, interleavedResult, separatedResult);
+            Matrices.interleave(context, interleavedResult, separatedResult);
             t3 = System.nanoTime();
             for (int k = 0; k < unpacked.size(); k++) {
                 checkIdentity(separatedResult.get(k), matrices.get(k), "Channel #" + k);
@@ -93,6 +95,21 @@ public class SliceMergeMatricesTest {
             System.out.printf("interleave in-place: %.3f ms, %.3f MB/sec%n",
                     (t2 - t1) * 1e-6, Matrices.sizeOf(interleaved) / 1048576.0 / ((t2 - t1) * 1e-9));
             System.out.printf("separate in-place: %.3f ms, %.3f MB/sec%n",
+                    (t3 - t2) * 1e-6, Matrices.sizeOf(interleaved) / 1048576.0 / ((t3 - t2) * 1e-9));
+
+            final Matrix<UpdatablePArray> separatedSingle = MEMORY_MODEL.newMatrix(UpdatablePArray.class,
+                    interleaved.elementType(),
+                    interleaved.dim(1), interleaved.dim(2), interleaved.dim(0));
+            t1 = System.nanoTime();
+            Matrices.separate(context, Matrices.asLayers(separatedSingle), interleaved);
+            t2 = System.nanoTime();
+            Matrices.interleave(context, interleavedResult, Matrices.asLayers(separatedSingle));
+            t3 = System.nanoTime();
+            checkIdentity(separatedSingle, merged, "Separated-in-single");
+            checkIdentity(interleavedResult, interleaved, "Interleaved-in-single");
+            System.out.printf("interleave in single matrix: %.3f ms, %.3f MB/sec%n",
+                    (t2 - t1) * 1e-6, Matrices.sizeOf(interleaved) / 1048576.0 / ((t2 - t1) * 1e-9));
+            System.out.printf("separate in single matrix: %.3f ms, %.3f MB/sec%n",
                     (t3 - t2) * 1e-6, Matrices.sizeOf(interleaved) / 1048576.0 / ((t3 - t2) * 1e-9));
         }
     }
